992cfb35d8bc0d3ed76a2560b3f184f11ee2df30
Nimittxo <akki042005@gmail.com>
2024-08-31 00:58:08 +0530

    Create StackIntroduction.c

---
+++ C/DataStructures/Stacks/StackIntroduction.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAX_SIZE 100
+
+int main(char* argc[], int argv){
+    return 0;
+}
+// Push() function: 
+/* Printf("Enter element to enter: ")
+   scanf("%d", &x)
+   if(top == MAX_SIZE-1)
+   return -1
+   else:
+   top = top+1
+   stack(top = x)*/
+
+// Pop() function:
+/*  Printf("Enter element to pop: ")
+    scanf("%d", &x)
+    if(top == -1)
+    return -1
+    else: 
+    (top -1)
+*/
+
+// Peek() function:
+/*
+
+*/
